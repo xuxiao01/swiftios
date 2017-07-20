b021b11babdd82c2a160ba98bffaa51316b41bbc
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2017-07-20 09:48:15 +0800

    upload

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swift-ios' do
+
+  pod 'MobileCenter'
+  
+end
+
+  target 'swift-iosTests' do
+  
+  end
+
+  target 'swift-iosUITests' do
+  
+  
+
+end
---
+++ swift-ios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  swift-ios
+//
+//  Created by bys on 19/07/2017.
+//  Copyright © 2017 bys. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
